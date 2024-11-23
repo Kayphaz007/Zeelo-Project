8438719a68d47f7db7c48a8e8f3f9dbb0d4edf6e
Kayphaz007 <kayphaz007@gmail.com>
2024-11-23 21:34:41 +0000

    select date and time

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from utils.mouseActions import click
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from utils.utilsFunctions import enterPasswordTwice
 
@@ -21,7 +20,6 @@ wait = WebDriverWait(driver, 10)
 # driver = webdriver.Chrome(options=options)
 driver.get(zeeloUrl)
 # alert = driver.switch_to.alert
-# TODO write if condition i.e if there is a popup to accept cookies
 try:
     accept_alert = wait.until(
         EC.element_to_be_clickable(
@@ -56,6 +54,135 @@ click(driver, elem_continue)
 driver.implicitly_wait(3)
 # enter password twice
 enterPasswordTwice(driver, password)
+
+
+# click Manage your passes
+
+elem_continue = driver.find_element(
+    By.XPATH, "//div[@data-testid='my_rides__travel_pass[0]']"
+)
+
+click(driver, elem_continue)
+
+# TODO select specific date
+# click a date
+
+try:
+    # TODO apply wait.until to all elements found
+    elem_continue = wait.until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "iMeCJE"))
+    )
+    # elem_continue = wait.until(
+    #     EC.element_to_be_clickable(
+    #         (By.XPATH, "//button[@id='hs-eu-confirmation-button']")
+    #     )
+    # )
+
+    click(driver, elem_continue)
+
+except Exception as error:
+    print(f"Error: {error}")
+
+# click outbound select times
+try:
+    accept_alert = wait.until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "//button[@data-testid='manage_travel_plans_modal__outbound__select_times_btn']",
+            )
+        )
+    )
+    # accept_alert = driver.find_element(
+    #     By.XPATH, "//button[@id='hs-eu-confirmation-button']"
+    # )
+
+    click(driver, accept_alert)
+
+except Exception as error:
+    print(f"Error: {error}")
+
+# select time
+try:
+    elem_continue = wait.until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "//div[@data-testid='manage_travel_plans_modal__outbound__time']//div//select",
+            )
+        )
+    )
+
+    selectElem = Select(elem_continue)
+    selectElem.select_by_visible_text("08:35")
+
+except Exception as error:
+    print(f"Error: {error}")
+
+
+# click inbound select times
+
+try:
+    accept_alert = wait.until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "//button[@data-testid='manage_travel_plans_modal__inbound__select_times_btn']",
+            )
+        )
+    )
+    # accept_alert = driver.find_element(
+    #     By.XPATH, "//button[@id='hs-eu-confirmation-button']"
+    # )
+
+    click(driver, accept_alert)
+
+except Exception as error:
+    print(f"Error: {error}")
+
+# select time
+try:
+    elem_continue = wait.until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "//div[@data-testid='manage_travel_plans_modal__inbound__time']//div//select",
+            )
+        )
+    )
+
+    selectElem = Select(elem_continue)
+    selectElem.select_by_visible_text("16:15")
+
+except Exception as error:
+    print(f"Error: {error}")
+
+# click confirm booking
+try:
+    elem_continue = wait.until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "//button[@data-testid='manage_travel_plans_modal__submit_btn']",
+            )
+        )
+    )
+
+    click(driver, elem_continue)
+
+except Exception as error:
+    print(f"Error: {error}")
+
+# response will be sent.... "sorry, there are not enough seats available"
+
+# repeat the above until successfull
+
+# if successfull
+
+# TODO Select all necessary days
+# repeat booking page opens
+
+
 # elem.clear()
 print("hold")
 # elem.send_keys("pycon")
